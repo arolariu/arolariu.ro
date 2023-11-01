729b13138c65be62fbf8f748356ef7be52f16a8a
Alexandru-Razvan Olariu (from Dev Box) <aolariu@microsoft.com>
2023-11-01 17:32:20 +0200

    worked on common package library

---
+++ sites/api.arolariu.ro/src/Common/Validators/Throw.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace arolariu.Backend.Common.Validators;
+
+[ExcludeFromCodeCoverage]
+internal static class Throw<T>
+    where T: Exception
+{
+    public static void IfNull(
+        #nullable enable
+        object? value,
+        string? message = null
+        #nullable disable
+        )
+    {
+        if (value is null)
+        {
+            var exception = (T)Activator.CreateInstance(typeof(T), message);
+            throw exception;
+        }
+    }
+}
