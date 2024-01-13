5d71bcacf88da2257b9416d78cc73ff5e961808d
Alexandru Olariu <arolariu@Mikis-MacBook-Air.local>
2024-01-13 09:37:38 +0200

    work on account stub

--- sites/arolariu.ro/src/app/accounts/[account]/page.tsx
+++ sites/arolariu.ro/src/app/accounts/[account]/page.tsx
@@ -1,3 +1,4 @@
+import fetchUser from "@/lib/fetchUser";
 
 interface Props {
     params: {
@@ -5,10 +6,16 @@ interface Props {
     }
 }
 export default async function AccountPage({params}: Readonly<Props>) {
+    const {user, isAuthenticated} = await fetchUser();
+    
     return (
         <main className="m-20">
             <section>
+            <h1> You are {isAuthenticated ? "authenticated" : "not authenticated"}. </h1>
                 <h1> Your account is {params.account} </h1>
+                <h1> Your user is {user?.id} </h1>
+
+                <h2> The user id and the account are: {user?.id === params.account ? "the same" : "different"} </h2>
             </section>
         </main>
     )
