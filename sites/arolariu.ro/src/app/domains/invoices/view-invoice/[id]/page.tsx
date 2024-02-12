e69fccf8bc02f2b29b7d0749a98d630df1493253
Alexandru-Razvan Olariu <aolariu@microsoft.com>
2024-02-12 22:00:20 +0200

    refactored view-invoice page

--- sites/arolariu.ro/src/app/domains/invoices/view-invoice/[id]/page.tsx
+++ sites/arolariu.ro/src/app/domains/invoices/view-invoice/[id]/page.tsx
@@ -4,6 +4,7 @@ import fetchInvoice from "@/lib/invoices/fetchInvoice";
 import {Metadata} from "next";
 import {RenderViewInvoicePage} from "./island";
 import Image from "next/image";
+import fetchUser from "@/lib/fetchUser";
 
 interface Props { params: {id: string}; }
 
@@ -12,13 +13,11 @@ export const metadata: Metadata = {
 	description: "View your uploaded invoice on `arolariu.ro`.",
 };
 
-export default async function ViewInvoicePage({params}: Readonly<Props>) {
-	const invoice = await fetchInvoice(params.id);
+export default async function ViewInvoicePage({ params }: Readonly<Props>) {
+	const { user } = await fetchUser();
+	const invoice = await fetchInvoice(params.id, user);
+	
 	if (!invoice) { return <Image src="/images/domains/invoices/403.svg" alt="Forbidden SVG" width="500" height="500"/> }
-
-	// TODO: check if invoice was shared with the user.
-	// TODO: shared keys...? hardcoded attribute?
-
 	return (
 		<section className="overflow-hidden dark:text-gray-300">
 			<RenderViewInvoicePage invoice={invoice} />
--- sites/arolariu.ro/src/lib/invoices/fetchInvoice.ts
+++ sites/arolariu.ro/src/lib/invoices/fetchInvoice.ts
@@ -1,21 +1,32 @@
 import Invoice from "@/types/invoices/Invoice";
-import { API_URL } from "../../constants";
-import fetchUser from "../fetchUser";
+import {User} from "@clerk/backend";
+import {API_URL} from "../../constants";
 import generateJWT from "../generateJWT";
 
-export default async function fetchInvoice(id:string) : Promise<Invoice | null>{
-    const {user, isAuthenticated} = await fetchUser();
-    const userIdentifier = isAuthenticated ? user?.id : "00000000-0000-0000-0000-000000000000";
-    const userAuthorization = isAuthenticated ? user?.id : await generateJWT(); // TODO: check JWT.
+export default async function fetchInvoice(id: string, user: User | null): Promise<Invoice | null> {
+	const authorizationHeader = await generateJWT(user);
+	const headers = {
+		"Content-Type": "application/json",
+		Authorization: `Bearer ${authorizationHeader}`,
+	};
 
-    const response = await fetch(`${API_URL}/users/${userIdentifier}/invoices/${id}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${userAuthorization}`,
-        }
+	// To understand if the invoice was shared with the user, we will hit the API on both the user's behalf and the guest's behalf.
+	// If the invoice is found on the user's behalf, we will return it. Otherwise, we will check if we get a good response from the guest.
+	// If neither of the requests return a success, we will return null.
+
+	const userResponse = await fetch(`${API_URL}/rest/user/${user?.id}/invoices/${id}`, {
+		method: "GET",
+		headers: headers,
     });
+    if (userResponse.status === 200) { return await userResponse.json(); }
 
-    if (response.status === 200) {return await response.json();}
-    else {return null;} // TODO: perform 403, 401, 500, etc. handling
+
+	const guestResponse = await fetch(`${API_URL}/rest/invoices/${id}`, {
+		method: "GET",
+		headers: headers,
+	});
+    if (guestResponse.status === 200) { return await guestResponse.json(); }
+
+
+    else { return null; } // TODO: perform 403, 401, 500, etc. handling
 }
