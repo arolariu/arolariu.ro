831f7d8dcc9f40b07cd9ba2ae0812394c697e954
Alexandru Olariu <arolariu@Mikis-MacBook-Air.local>
2024-02-17 01:38:50 +0200

    refactored components folder

--- sites/arolariu.ro/src/components/general/Accordion.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from "react";
-import AccordionItem from "./AccordionItem";
-
-// TODO: enforce that children is of type AccordionItem (currently doesn't work)
-interface Props { children: ReactElement<typeof AccordionItem> | ReactElement<typeof AccordionItem>[]; }
-
-export default function Accordion({children}: Readonly<Props>) {
-	return <div className="bg-white border border-gray-100 divide-y divide-gray-100 rounded-xl">{children}</div>;
-}
