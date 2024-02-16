273df2ba693d1899dd6ccfe79b57e1bfe06725bd
Alexandru Olariu <arolariu@Mikis-MacBook-Air.local>
2024-02-16 14:14:35 +0200

    finished about/the-platform page

--- sites/arolariu.ro/src/app/domains/invoices/create-invoice/page.tsx
+++ sites/arolariu.ro/src/app/domains/invoices/create-invoice/page.tsx
@@ -2,7 +2,6 @@
 
 import fetchUser from "@/lib/fetchUser";
 import {Metadata} from "next";
-import Link from "next/link";
 import RenderInvoiceScreen from "./island";
 
 export const metadata: Metadata = {
@@ -19,19 +18,7 @@ export default async function CreateInvoicePage() {
 			<RenderInvoiceScreen user={user}/>
 			{!isAuthenticated && (
 				<div className="flex flex-col mx-auto mb-32">
-					<p className="mb-4 text-2xl text-center">In order to save your invoice, please create an account or sign in.</p>
-					<div className="flex flex-row gap-4 mx-auto">
-					<Link href="/auth/sign-up">
-						<button className="mx-auto btn btn-secondary" type="button">
-							Create an account
-						</button>
-					</Link>
-					<Link href="/auth/sign-in">
-						<button className="mx-auto btn btn-primary" type="button">
-							Sign in
-						</button>
-						</Link>
-						</div>
+					<p className="mb-4 text-center 2xsm:text-md md:text-2xl">In order to save your invoice, please create an account or sign in.</p>
 				</div>
 			)}
 		</main>
