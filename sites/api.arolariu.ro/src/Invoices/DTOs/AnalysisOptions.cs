a448dedce4a6c29e87d2923603e5932be3e4a6d0
Alexandru-Razvan Olariu <aolariu@microsoft.com>
2024-04-07 01:53:12 +0300

    refactored analysis enum

---
+++ sites/api.arolariu.ro/src/Invoices/DTOs/AnalysisOptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace arolariu.Backend.Domain.Invoices.DTOs;
+
+/// <summary>
+/// This class represents the invoice analysis options DTO.
+/// </summary>
+[Serializable]
+public enum AnalysisOptions
+{
+    /// <summary>
+    /// No analysis.
+    /// </summary>
+    NoAnalysis,
+
+    /// <summary>
+    /// Do a complete analysis of the invoice.
+    /// </summary>
+    CompleteAnalysis,
+
+    /// <summary>
+    /// Analyze the invoice description.
+    /// </summary>
+    InvoiceOnly,
+
+    /// <summary>
+    /// Analyze the invoice items only.
+    /// </summary>
+    InvoiceItemsOnly,
+}
