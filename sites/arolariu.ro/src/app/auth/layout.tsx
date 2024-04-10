6e2698121a1c5f77db525963caf54de233bd98a9
Alexandru-Razvan Olariu <aolariu@microsoft.com>
2024-04-10 16:59:31 +0300

    Update layout.tsx
    Signed-off-by: Alexandru-Razvan Olariu <aolariu@microsoft.com>

--- sites/arolariu.ro/src/app/auth/layout.tsx
+++ sites/arolariu.ro/src/app/auth/layout.tsx
@@ -11,13 +11,3 @@ export default async function AuthRootLayout({children}: Readonly<PropsWithChild
     </main>
   );
 }
-=======
-import {type PropsWithChildren} from "react";
-
-export default async function AuthRootLayout({children}: Readonly<PropsWithChildren<{}>>) {
-  return (
-    <main className='flex flex-col flex-nowrap items-center justify-center justify-items-center gap-4 px-5 py-24 text-center lg:flex-row lg:gap-8'>
-      {children}
-    </main>
-  );
-}
