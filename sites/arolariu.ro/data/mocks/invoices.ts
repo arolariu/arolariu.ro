965bcb5c94b6e9f05e6143c3f78424a5607c6670
Alexandru Olariu <arolariu@Mikis-MacBook-Air.local>
2024-05-23 09:02:13 +0300

    refactored fakes to have better logic

--- sites/arolariu.ro/data/mocks/invoices.ts
+++ sites/arolariu.ro/data/mocks/invoices.ts
@@ -1,55 +1,61 @@
 /** @format */
 
-import Invoice, {InvoiceCategory, type Recipe} from "@/types/invoices/Invoice";
+import Invoice, {type InvoiceCategory, type Recipe} from "@/types/invoices/Invoice";
 import Merchant, {MerchantCategory} from "@/types/invoices/Merchant";
-import Product, {ItemCategory} from "@/types/invoices/Product";
+import Product, {Allergen, ItemCategory} from "@/types/invoices/Product";
 import {faker as fake} from "@faker-js/faker";
 
-const generateFakeInvoice = (): Invoice => ({
-  id: fake.string.uuid(),
-  category: fake.number.int({min: 1, max: 3}) as InvoiceCategory,
-  createdAt: fake.date.anytime(),
-  merchant: generateFakeMerchant(),
-  createdBy: fake.string.uuid(),
-  description: fake.lorem.sentence(6),
-  estimatedSurvivalDays: fake.number.int({min: 0, max: 10}),
-  isImportant: fake.datatype.boolean(),
-  isSoftDeleted: fake.datatype.boolean(),
-  lastUpdatedAt: fake.date.recent(),
-  lastUpdatedBy: fake.string.uuid(),
-  name: fake.lorem.sentence(5),
-  numberOfUpdates: fake.number.int({min: 0, max: 100}),
-  photoLocation: "https://cdn.arolariu.ro/fakes/" + fake.lorem.slug(3),
-  userIdentifier: fake.string.uuid(),
-  items: Array.from({length: fake.number.int({min: 1, max: 30})}, generateFakeInvoiceProduct),
-  paymentInformation: {
-    currency: {name: fake.finance.currencyName(), symbol: fake.finance.currencySymbol()},
-    dateOfPurchase: fake.date.past(),
-    totalAmount: fake.number.float({min: 0, max: 1000, multipleOf: 2}),
-    totalTax: fake.number.float({min: 0, max: 1000, multipleOf: 2}),
-  },
-  possibleRecipes: Array.from(
-    {length: fake.number.int({min: 0, max: 3})},
-    () =>
-      ({
-        name: fake.lorem.sentence(3),
-        complexity: fake.number.int({min: 1, max: 5}),
-        observations: [fake.lorem.sentence(5), fake.lorem.sentence(5)],
-        recipeIngredients: Array.from({length: fake.number.int({min: 1, max: 10})}, generateFakeInvoiceProduct),
-        duration: fake.date.future().toISOString(),
-      }) satisfies Recipe,
-  ),
-  additionalMetadata: Array.from(
-    {length: fake.number.int({min: 0, max: 2})},
-    () =>
-      ({
-        [fake.lorem.word(4)]: fake.lorem.sentence(5),
-      }) as unknown as Record<string, object>,
-  ),
-});
+const generateFakeInvoice = (): Invoice => {
+  const invoiceCategory = fake.number.int({min: 1, max: 3}) as InvoiceCategory;
+  const products = Array.from({length: fake.number.int({min: 1, max: 30})}, generateFakeInvoiceProduct);
+  const totalAmount = products.reduce((acc, product) => acc + product.totalPrice, 0);
+
+  return {
+    id: fake.string.uuid(),
+    category: invoiceCategory,
+    createdAt: fake.date.past(),
+    merchant: generateFakeMerchant(),
+    createdBy: fake.string.uuid(),
+    description: fake.lorem.sentence(7),
+    estimatedSurvivalDays: fake.number.int({min: totalAmount / 30, max: totalAmount / 10}),
+    isImportant: fake.datatype.boolean(),
+    isSoftDeleted: fake.datatype.boolean(),
+    lastUpdatedAt: fake.date.recent(),
+    lastUpdatedBy: fake.string.uuid(),
+    name: fake.lorem.sentence(3),
+    numberOfUpdates: fake.number.int({min: 0, max: 100}),
+    photoLocation: "https://cdn.arolariu.ro/fakes/" + fake.lorem.slug(3),
+    userIdentifier: fake.string.uuid(),
+    items: products,
+    paymentInformation: {
+      currency: {name: fake.finance.currencyName(), symbol: fake.finance.currencySymbol()},
+      dateOfPurchase: fake.date.past(),
+      totalAmount: totalAmount,
+      totalTax: fake.number.float({min: totalAmount * 0.05, max: totalAmount / 2, multipleOf: 3}),
+    },
+    possibleRecipes: Array.from(
+      {length: fake.number.int({min: 0, max: 3})},
+      () =>
+        ({
+          name: fake.lorem.sentence(3),
+          complexity: fake.number.int({min: 1, max: 5}),
+          observations: [fake.lorem.sentence(3), fake.lorem.sentence(2)],
+          recipeIngredients: products.filter(() => fake.datatype.boolean()),
+          duration: `${fake.number.int({min: 5, max: 120})} minutes`,
+        }) satisfies Recipe,
+    ),
+    additionalMetadata: Array.from(
+      {length: fake.number.int({min: 0, max: 2})},
+      () =>
+        ({
+          [fake.lorem.word(4)]: fake.lorem.sentence(5),
+        }) as unknown as Record<string, object>,
+    ),
+  } satisfies Invoice;
+};
 
 const generateFakeMerchant = (): Merchant => ({
-  category: MerchantCategory.LOCAL_SHOP,
+  category: fake.number.int({min: 1, max: 3}) as MerchantCategory,
   address: fake.location.streetAddress(true),
   name: fake.company.name(),
   createdAt: fake.date.past(),
@@ -65,22 +71,48 @@ const generateFakeMerchant = (): Merchant => ({
   phoneNumber: fake.phone.number(),
 });
 
-const generateFakeInvoiceProduct = (): Product => ({
-  category: fake.number.int({min: 0, max: 13}) as ItemCategory,
-  detectedAllergens: [{name: fake.lorem.word()}, {name: fake.lorem.word()}, {name: fake.lorem.word()}],
-  genericName: fake.commerce.productName(),
-  price: fake.number.float({min: -20, max: 1000, multipleOf: 2}),
-  productCode: fake.string.alphanumeric(8),
-  quantity: fake.number.int({min: 1, max: 100}),
-  rawName: fake.commerce.productName(),
-  quantityUnit: fake.commerce.productMaterial(),
-  totalPrice: fake.number.float({min: -200, max: 1000, multipleOf: 2}),
-  metadata: {
-    isComplete: fake.datatype.boolean(),
-    isEdited: fake.datatype.boolean(),
-    isSoftDeleted: fake.datatype.boolean(),
-  },
-});
+const generateFakeInvoiceProduct = (): Product => {
+  const allergensList = [
+    "gluten",
+    "crustaceans",
+    "eggs",
+    "fish",
+    "peanuts",
+    "soybeans",
+    "milk",
+    "nuts",
+    "celery",
+    "mustard",
+    "sesame seeds",
+    "sulphur dioxide",
+    "lupin",
+    "molluscs",
+  ];
+  const allergens = Array.from({length: fake.number.int({min: 0, max: 3})}, () => ({
+    name: fake.number.int({min: 0, max: 13}),
+  })).map((allergen) => ({name: allergensList[allergen.name]}));
+
+  const price = fake.number.float({min: 0, max: 1000, multipleOf: 2});
+  const quantity = fake.number.int({min: 1, max: 100});
+  const totalPrice = price * quantity;
+
+  return {
+    category: fake.number.int({min: 0, max: 13}) as ItemCategory,
+    detectedAllergens: [...allergens] as Allergen[],
+    genericName: fake.commerce.productName(),
+    price,
+    productCode: fake.string.alphanumeric(8),
+    quantity,
+    rawName: fake.commerce.productName(),
+    quantityUnit: fake.commerce.productMaterial(),
+    totalPrice,
+    metadata: {
+      isComplete: fake.datatype.boolean(),
+      isEdited: fake.datatype.boolean(),
+      isSoftDeleted: fake.datatype.boolean(),
+    },
+  } satisfies Product;
+};
 
 export const FakeInvoice: Invoice = generateFakeInvoice();
 export const FakeInvoiceShortList: Invoice[] = Array.from(
