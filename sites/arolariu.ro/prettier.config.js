ea31dd608e56ec8d129f3f9b814ee87f13dd00d9
Alexandru Olariu <arolariu@Mikis-MacBook-Air.local>
2024-05-25 11:11:58 +0300

    fixed prettier config

--- sites/arolariu.ro/prettier.config.js
+++ sites/arolariu.ro/prettier.config.js
@@ -2,27 +2,26 @@
  * @format
  * @type {import("prettier").Config}
  */
-
 const prettierConfig = {
   arrowParens: "always",
-  insertPragma: true,
-  printWidth: 120,
-  useTabs: false,
-  tabWidth: 2,
-  semi: true,
-  htmlWhitespaceSensitivity: "strict",
-  trailingComma: "all",
-  singleQuote: false,
   bracketSameLine: true,
   bracketSpacing: false,
-  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
   endOfLine: "crlf",
-  proseWrap: "always",
-  parser: "typescript",
-  jsxSingleQuote: true,
-  singleAttributePerLine: true,
-  tailwindConfig: "tailwind.config.ts",
   experimentalTernaries: false,
+  htmlWhitespaceSensitivity: "strict",
+  insertPragma: true,
+  jsxSingleQuote: true,
+  parser: "typescript",
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
+  printWidth: 120,
+  proseWrap: "always",
+  semi: true,
+  singleAttributePerLine: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+  tailwindConfig: "tailwind.config.ts",
 };
 
 export default prettierConfig;
