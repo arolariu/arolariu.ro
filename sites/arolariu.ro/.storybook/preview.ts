314a86181831050a5262496afb10513e31114aac
Alexandru Olariu <arolariu@Mikis-MacBook-Air.local>
2024-06-01 00:38:38 +0300

    added accessibility and tailwind theme switcher addons for storybook:

--- sites/arolariu.ro/.storybook/preview.ts
+++ sites/arolariu.ro/.storybook/preview.ts
@@ -1,7 +1,18 @@
 /** @format */
 
+import {withThemeByClassName} from "@storybook/addon-themes";
 import type {Preview} from "@storybook/react";
 import "../src/app/globals.css";
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: "light",
+      dark: "dark",
+    },
+    defaultTheme: "light",
+  }),
+];
 
 const preview: Preview = {
   parameters: {
