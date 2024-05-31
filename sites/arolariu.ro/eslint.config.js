abef67f64608aa735de589b8d4bac36bccf01102
Alexandru Olariu <arolariu@Mikis-MacBook-Air.local>
2024-05-31 19:36:13 +0300

    sorted eslint rules alphabetically

--- sites/arolariu.ro/eslint.config.js
+++ sites/arolariu.ro/eslint.config.js
@@ -59,92 +59,93 @@ const eslintConfig = tseslint.config(
       ...eslintPluginSonarJs.configs.recommended.rules,
       ...eslintPluginJsDoc.configs["recommended-typescript"].rules,
       ...eslintPluginPerfectionist.configs["recommended-alphabetical"].rules,
-      "jsdoc/require-param": "off",
-      "jsdoc/require-returns": "off",
-      "jsdoc/check-tag-names": "off", // prettier's @format tag is not recognized.
-      "jsdoc/check-param-names": "off",
-      "unicorn/filename-case": "off",
-      "unicorn/no-keyword-prefix": "off",
-      "react-hooks/exhaustive-deps": "off", // sometimes useEffect hooks depend on a prop that is not in the dependency array.
-      "unicorn/switch-case-braces": "off",
-      "unicorn/prevent-abbreviations": "off",
-      "unicorn/no-abusive-eslint-disable": "off",
-      "unicorn/no-array-reduce": "off", // array reduce is useful for reducing arrays and in FP.
-      "functional/prefer-immutable-types": "off", // typescript v5 does not offer a deep readonly type; TBD.
-      "unicorn/consistent-function-scoping": "off",
-      "perfectionist/sort-intersection-types": "off", // prettier sorts intersection types automatically.
-      "perfectionist/sort-jsx-props": "off", // prettier sorts JSX props automatically.
-      "functional/no-conditional-statements": "off", // conditional statements are used for conditional rendering.
-      "functional/functional-parameters": "off", // Not applicable to TSX functions.
-      "react/prop-types": "off",
-      "functional/no-let": "off", // let is useful for iterators and ahead-of-time computing.
-      "functional/no-return-void": "off", // TSX components do not always have a return statement.
-      "functional/no-mixed-types": "off", // TSX components have mixed types used for props.
-      "react/jsx-indent": "off",
-      "perfectionist/sort-object-types": "off", // prettier sorts object types automatically.
-      "perfectionist/sort-interfaces": "off", // prettier sorts interfaces automatically.
-      "functional/no-expression-statements": "off", // side-effects are needed by 3rd party libraries.
-      "react/jsx-newline": "off",
-      "perfectionist/sort-objects": "off", // prettier sorts objects automatically.
-      "react/jsx-no-bind": "off",
-      "perfectionist/sort-imports": "off", // prettier sorts imports automatically.
-      "perfectionist/sort-named-imports": "off", // prettier sorts named imports automatically.
-      "perfectionist/sort-union-types": "off", // prettier sorts union types automatically.
-      "react/jsx-max-depth": "off",
-      "react/no-multi-comp": "off",
-      "react/no-array-index-key": "off", // this is a good rule that we violate for some components.
-      "react/jsx-sort-props": "off",
-      "react/jsx-no-literals": "off", // this is a good rule for I18N.
-      "react/jsx-tag-spacing": "off",
-      "react/jsx-indent-props": "off",
-      "react/jsx-curly-newline": "off",
-      "react/react-in-jsx-scope": "off",
-      "id-length": "off", // mayfly variables like iterators tend to have 'i' or 'j' as iterator names.
-      "@typescript-eslint/no-non-null-assertion": "off", // this rule is not needed when working with TSX.
-      "react/require-default-props": "off",
-      "new-cap": "off",
-      radix: "off",
-      camelcase: "off",
-      "unicorn/no-null": "off",
-      "arrow-body-style": "off", // arrow body style is not needed; we use both arrow and function expressions.
-      "no-shadow": "off", // this rule seems to not behave well with TSX.
-      "consistent-return": "off", // TSX components do not always have a return statement.
-      "max-lines-per-function": "off", // TSX components are not always small.
-      "react/jsx-props-no-spreading": "off",
-      "react/jsx-max-props-per-line": "off",
-      "react/jsx-filename-extension": "off",
-      "sort-keys": "off", // prettier sorts keys automatically.
-      curly: "off", // curly braces are not needed for single-line blocks.
       "@typescript-eslint/ban-types": "warn", // substitute for `Don't use `{}` as a type. `{}` actually means "any non-nullish value".`
-      "react/forbid-component-props": "off",
-      "react/jsx-closing-tag-location": "off",
-      "react/jsx-child-element-spacing": "off",
-      "react/no-adjacent-inline-elements": "off",
-      "react/jsx-one-expression-per-line": "off",
-      "react/jsx-closing-bracket-location": "off",
-      "max-lines": "off",
-      "jsdoc/tag-lines": "off", // prettier automatically adds a newline after block description; this rule is not needed.
-      "no-shadow": "off",
-      "one-var": "off", // this rule is not needed; we have more than one variable per exported file.
-      "no-magic-numbers": "off", // this rule is not needed; we have magic numbers regarding request status in our code.
-      "sort-imports": "off", // prettier sorts imports automatically.
-      "no-inline-comments": "off", // inline comments are useful, sometimes.
-      "func-style": "off", // function style is not needed; we use both arrow and function expressions.
-      "capitalized-comments": "off", // capitalized comments are not needed; we have comments in lowercase.
-      "react/function-component-definition": "off",
-      "@typescript-eslint/require-await": "off",
-      "@typescript-eslint/non-nullable-type-assertion-style": "off",
-      "no-console": "off", // console logs are stripped out in production.
-      "@typescript-eslint/no-misused-promises": "off",
-      "no-plusplus": "off", // plusplus is useful for iterators.
-      "prefer-named-capture-group": "off",
-      "react/no-unstable-nested-components": "off", // next-intl (i18n) uses nested components for structured messages.
       "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/no-confusing-void-expression": "off",
+      "@typescript-eslint/no-misused-promises": "off",
+      "@typescript-eslint/no-non-null-assertion": "off", // this rule is not needed when working with TSX.
+      "@typescript-eslint/no-unsafe-assignment": "off", // unsafe assignment is useful for type casting.
+      "@typescript-eslint/non-nullable-type-assertion-style": "off",
+      "@typescript-eslint/require-await": "off",
+      "arrow-body-style": "off", // arrow body style is not needed; we use both arrow and function expressions.
+      "capitalized-comments": "off", // capitalized comments are not needed; we have comments in lowercase.
+      "consistent-return": "off", // TSX components do not always have a return statement.
+      "func-style": "off", // function style is not needed; we use both arrow and function expressions.
+      "functional/functional-parameters": "off", // Not applicable to TSX functions.
+      "functional/no-conditional-statements": "off", // conditional statements are used for conditional rendering.
+      "functional/no-expression-statements": "off", // side-effects are needed by 3rd party libraries.
+      "functional/no-let": "off", // let is useful for iterators and ahead-of-time computing.
+      "functional/no-mixed-types": "off", // TSX components have mixed types used for props.
+      "functional/no-return-void": "off", // TSX components do not always have a return statement.
+      "functional/prefer-immutable-types": "off", // typescript v5 does not offer a deep readonly type; TBD.
+      "id-length": "off", // mayfly variables like iterators tend to have 'i' or 'j' as iterator names.
+      "jsdoc/check-param-names": "off",
+      "jsdoc/check-tag-names": "off", // prettier's @format tag is not recognized.
+      "jsdoc/require-returns": "off",
+      "jsdoc/require-param": "off",
+      "jsdoc/tag-lines": "off", // prettier automatically adds a newline after block description; this rule is not needed.
+      "max-lines": "off",
+      "max-lines-per-function": "off", // TSX components are not always small.
+      "new-cap": "off",
+      "no-console": "off", // console logs are stripped out in production.
+      "no-inline-comments": "off", // inline comments are useful, sometimes.
+      "no-magic-numbers": "off", // this rule is not needed; we have magic numbers regarding request status in our code.
+      "no-plusplus": "off", // plusplus is useful for iterators.
+      "no-shadow": "off",
+      "no-shadow": "off", // this rule seems to not behave well with TSX.
       "no-ternary": "off",
       "no-void": "off",
-      "@typescript-eslint/no-confusing-void-expression": "off",
       "no-warning-comments": "warn",
+      "one-var": "off", // this rule is not needed; we have more than one variable per exported file.
+      "perfectionist/sort-imports": "off", // prettier sorts imports automatically.
+      "perfectionist/sort-interfaces": "off", // prettier sorts interfaces automatically.
+      "perfectionist/sort-intersection-types": "off", // prettier sorts intersection types automatically.
+      "perfectionist/sort-jsx-props": "off", // prettier sorts JSX props automatically.
+      "perfectionist/sort-named-imports": "off", // prettier sorts named imports automatically.
+      "perfectionist/sort-object-types": "off", // prettier sorts object types automatically.
+      "perfectionist/sort-objects": "off", // prettier sorts objects automatically.
+      "perfectionist/sort-union-types": "off", // prettier sorts union types automatically.
+      "prefer-named-capture-group": "off",
+      "react-hooks/exhaustive-deps": "off", // sometimes useEffect hooks depend on a prop that is not in the dependency array.
+      "react/forbid-component-props": "off",
+      "react/function-component-definition": "off",
+      "react/jsx-child-element-spacing": "off",
+      "react/jsx-closing-bracket-location": "off",
+      "react/jsx-closing-tag-location": "off",
+      "react/jsx-curly-newline": "off",
+      "react/jsx-filename-extension": "off",
+      "react/jsx-indent": "off",
+      "react/jsx-indent-props": "off",
+      "react/jsx-max-depth": "off",
+      "react/jsx-max-props-per-line": "off",
+      "react/jsx-newline": "off",
+      "react/jsx-no-bind": "off",
+      "react/jsx-no-literals": "off", // this is a good rule for I18N.
+      "react/jsx-one-expression-per-line": "off",
       "react/jsx-pascal-case": "off",
+      "react/jsx-props-no-spreading": "off",
+      "react/jsx-sort-props": "off",
+      "react/jsx-tag-spacing": "off",
+      "react/no-adjacent-inline-elements": "off",
+      "react/no-array-index-key": "off", // this is a good rule that we violate for some components.
+      "react/no-multi-comp": "off",
+      "react/no-unstable-nested-components": "off", // next-intl (i18n) uses nested components for structured messages.
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/require-default-props": "off",
+      "sort-imports": "off", // prettier sorts imports automatically.
+      "sort-keys": "off", // prettier sorts keys automatically.
+      "unicorn/consistent-function-scoping": "off",
+      "unicorn/filename-case": "off",
+      "unicorn/no-abusive-eslint-disable": "off",
+      "unicorn/no-array-reduce": "off", // array reduce is useful for reducing arrays and in FP.
+      "unicorn/no-keyword-prefix": "off",
+      "unicorn/no-null": "off",
+      "unicorn/prevent-abbreviations": "off",
+      "unicorn/switch-case-braces": "off",
+      camelcase: "off",
+      curly: "off", // curly braces are not needed for single-line blocks.
+      radix: "off",
     },
     settings: {
       react: {
@@ -174,15 +175,7 @@ const eslintConfig = tseslint.config(
 
 // Add the global ignores to the default config.
 eslintConfig.forEach((config) => {
-  const ignoreList = [
-    "*.config.{js,ts}",
-    "**/node_modules/**",
-    "**/ui/**",
-    "**/i18n.ts",
-    "**/sw.ts",
-    "**/metadata.ts",
-    "**/middleware.ts",
-  ];
+  const ignoreList = ["*.config.{js,ts}", "**/node_modules/**", "**/ui/**", "**/*.ts"];
   config.ignores = config.ignores ? [...config.ignores, ...ignoreList] : [...ignoreList];
 });
 
