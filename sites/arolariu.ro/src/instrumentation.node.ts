7cf5ffb74160e64a7e62282d094ca3790b5a677a
Alexandru-Razvan Olariu <aolariu@microsoft.com>
2024-06-25 17:48:27 +0300

    switched otel from vercel to standard otel lib

---
+++ sites/arolariu.ro/src/instrumentation.node.ts
@@ -0,0 +1,15 @@
+/** @format */
+
+import {OTLPTraceExporter} from "@opentelemetry/exporter-trace-otlp-http";
+import {Resource} from "@opentelemetry/resources";
+import {NodeSDK} from "@opentelemetry/sdk-node";
+import {SimpleSpanProcessor} from "@opentelemetry/sdk-trace-node";
+import {SEMRESATTRS_SERVICE_NAME} from "@opentelemetry/semantic-conventions";
+
+const sdk = new NodeSDK({
+  resource: new Resource({
+    [SEMRESATTRS_SERVICE_NAME]: "arolariu-ro",
+  }),
+  spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
+});
+sdk.start();
