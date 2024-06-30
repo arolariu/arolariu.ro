f1189c353513dd969056d64fa772609dd1d84a2c
Alexandru Olariu <arolariu@Mikis-MacBook-Air.local>
2024-06-30 21:01:49 +0300

    optimistic UI for view-invoices page

--- sites/arolariu.ro/src/hooks/stateStore.tsx
+++ sites/arolariu.ro/src/hooks/stateStore.tsx
@@ -9,12 +9,14 @@ import {createJSONStorage, devtools, persist} from "zustand/middleware";
 
 type States = {
   selectedInvoice: Invoice;
+  invoices: Invoice[];
 };
 
 type Actions = {
   // Invoice actions:
   setSelectedInvoice: (invoice: Readonly<Invoice>) => void;
   removeSelectedInvoice: (invoice: Readonly<Invoice>) => void;
+  setInvoices: (invoices: Invoice[]) => void;
 };
 
 export const useZustandStore = create<States & Actions>()(
@@ -22,8 +24,10 @@ export const useZustandStore = create<States & Actions>()(
     persist(
       (set) => ({
         selectedInvoice: {} as Invoice,
+        invoices: [],
         setSelectedInvoice: (invoice: Readonly<Invoice>) => set({selectedInvoice: invoice}),
         removeSelectedInvoice: () => set({selectedInvoice: {} as Invoice}),
+        setInvoices: (invoices: Invoice[]) => set({invoices}),
       }),
       {name: "zustand-store", storage: createJSONStorage(() => sessionStorage)},
     ),
