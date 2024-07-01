67c43cc9d3254ade0de84cb579a29ee4ca6a389a
Alexandru-Razvan Olariu (from Dev Box) <aolariu@microsoft.com>
2024-07-01 22:44:22 +0300

    simplify zustand store

--- sites/arolariu.ro/src/hooks/stateStore.tsx
+++ sites/arolariu.ro/src/hooks/stateStore.tsx
@@ -8,14 +8,10 @@ import {create} from "zustand";
 import {createJSONStorage, devtools, persist} from "zustand/middleware";
 
 type States = {
-  selectedInvoice: Invoice;
   invoices: Invoice[];
 };
 
 type Actions = {
-  // Invoice actions:
-  setSelectedInvoice: (invoice: Readonly<Invoice>) => void;
-  removeSelectedInvoice: (invoice: Readonly<Invoice>) => void;
   setInvoices: (invoices: Invoice[]) => void;
 };
 
@@ -23,10 +19,7 @@ export const useZustandStore = create<States & Actions>()(
   devtools(
     persist(
       (set) => ({
-        selectedInvoice: {} as Invoice,
         invoices: [],
-        setSelectedInvoice: (invoice: Readonly<Invoice>) => set({selectedInvoice: invoice}),
-        removeSelectedInvoice: () => set({selectedInvoice: {} as Invoice}),
         setInvoices: (invoices: Invoice[]) => set({invoices}),
       }),
       {name: "zustand-store", storage: createJSONStorage(() => sessionStorage)},
