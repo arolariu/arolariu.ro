b49591f93a589a16d8fcbeb15bd69336fd0c4726
Alexandru-Razvan Olariu (from Dev Box) <aolariu@microsoft.com>
2024-07-29 17:21:46 +0300

    restructured EULA

--- sites/arolariu.ro/src/components/EULA.tsx
+++ sites/arolariu.ro/src/components/EULA.tsx
@@ -12,11 +12,13 @@ import {useTranslations} from "next-intl";
 import {useEffect, useState} from "react";
 import {Checkbox} from "./ui/checkbox";
 import {Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger} from "./ui/dialog";
+import {Separator} from "./ui/separator";
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "./ui/tabs";
 
 /**
  * The terms of service dialog component.
  */
-function TermsOfService() {
+function TermsOfServiceDialogButton() {
   const t = useTranslations("termsOfService");
   return (
     <Dialog>
@@ -38,7 +40,7 @@ function TermsOfService() {
 /**
  * The privacy policy dialog component.
  */
-function PrivacyPolicy() {
+function PrivacyPolicyDialogButton() {
   const t = useTranslations("privacyPolicy");
   return (
     <Dialog>
@@ -69,74 +71,71 @@ function CookiesBanner({
   setCookiesState: (state: {essential: boolean; analytics: boolean}) => void | Promise<void>;
 }>) {
   const t = useTranslations("EULA.cookiesPolicy");
+  const essentialsDescription = t.rich("cookies.essential.description", {
+    br: (chunks) => (
+      <>
+        {" "}
+        <br /> {chunks}{" "}
+      </>
+    ),
+  });
+
+  const analyticsDescription = t.rich("cookies.analytics.description", {
+    br: (chunks) => (
+      <>
+        {" "}
+        <br /> {chunks}{" "}
+      </>
+    ),
+    em: (chunks) => <em>{chunks}</em>,
+  });
 
   return (
-    <Card className='m-4'>
-      <CardHeader className='mb-4 border-b border-gray-300 pb-4'>
-        <div className='items-center'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='currentColor'
-            strokeWidth='2'
-            strokeLinecap='round'
-            className='mr-2 inline'
-            strokeLinejoin='round'>
-            <path d='M12 2a10 10 0 1 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5' />
-            <path d='M8.5 8.5v.01' />
-            <path d='M16 15.5v.01' />
-            <path d='M12 12v.01' />
-            <path d='M11 17v.01' />
-            <path d='M7 14v.01' />
-          </svg>
-          <CardTitle className='inline tracking-wide'>{t("title")}</CardTitle>
-          <CardDescription>{t("subtitle")}</CardDescription>
-        </div>
+    <Card className='m-4 flex flex-col items-center justify-center justify-items-center bg-white dark:bg-black'>
+      <CardHeader className='mb-4 items-center justify-center justify-items-center pb-4'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+          fill='none'
+          stroke='currentColor'
+          strokeWidth='2'
+          strokeLinecap='round'
+          className='m-2'
+          strokeLinejoin='round'>
+          <path d='M12 2a10 10 0 1 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5' />
+          <path d='M8.5 8.5v.01' />
+          <path d='M16 15.5v.01' />
+          <path d='M12 12v.01' />
+          <path d='M11 17v.01' />
+          <path d='M7 14v.01' />
+        </svg>
+        <CardTitle className='tracking-wide'>{t("title")}</CardTitle>
+        <CardDescription>{t("subtitle")}</CardDescription>
       </CardHeader>
-      <CardContent>
-        <form className='flex flex-col flex-wrap gap-4'>
-          <div>
+      <CardContent className='flex flex-col flex-wrap items-center justify-center justify-items-center gap-4'>
+        <form>
+          <div className='pb-4'>
             {t("cookies.essential.title")}
-            <small className='mb-2 block leading-tight tracking-wider'>
-              {t.rich("cookies.essential.description", {
-                br: (chunks) => (
-                  <>
-                    <br />
-                    {chunks}
-                  </>
-                ),
-              })}
-            </small>
+            <small className='mb-2 block leading-tight tracking-wider'>{essentialsDescription}</small>
             <Checkbox
               id='essential'
-              className='size-5'
+              className='size-6'
               checked={cookiesState.essential}
               disabled
             />
             <Label
               htmlFor='essential'
-              className='ml-2 text-gray-400'>
+              className='ml-2 text-gray-700'>
               {t("cookies.essential.checkbox")}
             </Label>
           </div>
-          <div>
+          <div className='pt-4'>
             {t("cookies.analytics.title")}
-            <small className='mb-2 block leading-tight tracking-wider'>
-              {t.rich("cookies.analytics.description", {
-                br: (chunks) => (
-                  <>
-                    <br />
-                    {chunks}
-                  </>
-                ),
-                em: (chunks) => <em className='text-gray-400'>{chunks}</em>,
-              })}
-            </small>
+            <small className='mb-2 block leading-tight tracking-wider'>{analyticsDescription}</small>
             <Checkbox
-              className='size-5'
+              className='size-6'
               onClick={() => setCookiesState({...cookiesState, analytics: !cookiesState.analytics})}
               checked={cookiesState.analytics}
               id='analytics'
@@ -159,6 +158,15 @@ function CookiesBanner({
  */
 export default function EULA() {
   const t = useTranslations("EULA");
+  const subtitle = t.rich("content", {
+    br: (chunks) => (
+      <>
+        {" "}
+        <br /> {chunks}{" "}
+      </>
+    ),
+  });
+
   const [locale, setLocale] = useState<string>("en");
   const [eulaAccepted, setEulaAccepted] = useState<boolean | null>(null);
   const [cookiesState, setCookiesState] = useState({
@@ -185,40 +193,37 @@ export default function EULA() {
     setEulaAccepted(true);
   };
 
-  return (
-    <main className='flex flex-col flex-nowrap items-center justify-center justify-items-center overflow-y-scroll bg-black py-24 text-center'>
-      <section className='rounded-xl bg-white shadow-inner shadow-black 2xsm:w-full lg:w-2/3'>
-        <h1 className='pt-2 text-center text-2xl font-bold text-black underline'>{t("title")}</h1>
+  const setLocaleHandler = (locale: string) => {
+    void setCookie({name: "locale", value: locale});
+    setLocale(locale);
+  };
 
-        <div className='flex justify-center gap-4'>
-          <p
-            onClick={() => {
-              setLocale("en");
-              void setCookie({name: "locale", value: "en"});
-            }}
+  return (
+    <main className='flex flex-col flex-nowrap items-center justify-center justify-items-center overflow-y-scroll py-24 text-center'>
+      <section className='rounded-xl border-2 2xsm:w-full lg:w-2/3'>
+        <h1 className='py-4 text-center text-2xl font-bold underline'>{t("title")}</h1>
+
+        <div className='flex flex-row flex-nowrap items-center justify-center justify-items-center gap-4'>
+          <Button
+            variant='default'
+            onClick={() => setLocaleHandler("en")}
             className={locale === "en" ? "bg-blue-500 text-white" : "bg-white text-black"}>
-            EN
-          </p>
-          <div className='divider divider-horizontal w-1 rounded-xl bg-black' />
-          <p
-            onClick={() => {
-              setLocale("ro");
-              void setCookie({name: "locale", value: "ro"});
-            }}
+            ENGLISH
+          </Button>
+          <Separator
+            orientation='vertical'
+            className='h-6 w-1 rounded bg-black dark:bg-white'
+          />
+          <Button
+            variant='default'
+            onClick={() => setLocaleHandler("ro")}
             className={locale === "ro" ? "bg-blue-500 text-white" : "bg-white text-black"}>
-            RO
-          </p>
+            ROMÂNĂ
+          </Button>
         </div>
 
-        <article className='mt-4 text-center text-gray-600'>
-          {t.rich("content", {
-            br: (chunks) => (
-              <>
-                <br />
-                {chunks}
-              </>
-            ),
-          })}
+        <article className='mt-4 text-center'>
+          {subtitle}
           <ul className='flex list-inside list-disc flex-col items-center'>
             <li>{t("termsOfService")}</li>
             <li>{t("privacyPolicy")}</li>
@@ -226,14 +231,37 @@ export default function EULA() {
         </article>
 
         <div className='justify-items-centers flex flex-row items-center justify-center gap-4'>
-          <TermsOfService />
-          <PrivacyPolicy />
+          <TermsOfServiceDialogButton />
+          <PrivacyPolicyDialogButton />
         </div>
-        <hr />
-        <CookiesBanner
-          cookiesState={cookiesState}
-          setCookiesState={setCookiesState}
-        />
+      </section>
+      <br />
+      <section className='rounded-xl border-2 2xsm:w-full lg:w-2/3'>
+        <Tabs
+          defaultValue='cookies'
+          className='py-4'>
+          <TabsList className='flex flex-row flex-nowrap items-center justify-center justify-items-center gap-4 bg-white dark:bg-black'>
+            <TabsTrigger value='cookies'>
+              <Button>Cookies Preference</Button>
+            </TabsTrigger>
+            <TabsTrigger value='settings'>
+              <Button>Additional Settings</Button>
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value='cookies'>
+            <CookiesBanner
+              cookiesState={cookiesState}
+              setCookiesState={setCookiesState}
+            />
+          </TabsContent>
+          <TabsContent value='settings'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate perspiciatis voluptates culpa natus qui
+            dicta iure beatae. Necessitatibus ipsam enim at maiores, odio minima ratione!
+          </TabsContent>
+        </Tabs>
+      </section>
+
+      <section>
         <div className='my-8 flex justify-center'>
           <Button
             variant='default'
