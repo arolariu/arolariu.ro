92d878bc13e0ba17a577155f683cec18d13cb6d0
Alexandru-Razvan Olariu (from Dev Box) <aolariu@microsoft.com>
2024-09-09 14:36:47 +0300

    removed OTel since it is unstable

--- sites/arolariu.ro/src/instrumentation.ts
+++ sites/arolariu.ro/src/instrumentation.ts
@@ -7,7 +7,8 @@
 export async function register() {
   if (process.env["NODE_ENV"] !== "development") {
     if (process.env["NEXT_RUNTIME"] === "nodejs") {
-      await import("./instrumentation.node.ts");
+      // Register the instrumentation for the Node.js runtime
     }
   }
 }
+
