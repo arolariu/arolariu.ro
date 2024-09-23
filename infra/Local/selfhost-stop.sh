12cd25a20bff6e19d33517e86b879ffcaed7b918
Alexandru-Razvan Olariu (from Dev Box) <aolariu@microsoft.com>
2024-09-23 14:28:13 +0300

    onprem docker setup - initial

---
+++ infra/Local/selfhost-stop.sh
@@ -0,0 +1,9 @@
+# This script will stop the docker containers that were started by the selfhost-start.sh script.
+
+# Stop the Storage container
+echo "📦 Preparing to stop the storage container..."
+docker-compose -f Storage/docker-compose.yml down
+
+# Stop the Management container
+echo "📦 Preparing to stop the management container..."
+docker-compose -f Management/docker-compose.yml down
