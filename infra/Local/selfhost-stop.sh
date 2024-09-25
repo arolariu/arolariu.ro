df33f69d9fd501d6e692b9c6f7ab87c27f047d7e
Alexandru-Razvan Olariu (from Dev Box) <aolariu@microsoft.com>
2024-09-25 12:47:33 +0300

    initial docker compose work for local infra

--- infra/Local/selfhost-stop.sh
+++ infra/Local/selfhost-stop.sh
@@ -2,8 +2,8 @@
 
 # Stop the Storage container
 echo "📦 Preparing to stop the storage container..."
-docker-compose -f Storage/docker-compose.yml down
+docker compose -f Storage/docker-compose.yml down
 
 # Stop the Management container
 echo "📦 Preparing to stop the management container..."
-docker-compose -f Management/docker-compose.yml down
+docker compose -f Management/docker-compose.yml down
