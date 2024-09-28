9ac2e2e63f2c29a9219802c22f1a200ff2a71ea8
Alexandru-Razvan Olariu (from Dev Box) <aolariu@microsoft.com>
2024-09-28 11:29:08 +0300

    reduce SBOM footprint by removing unused MDX packages

--- sites/arolariu.ro/src/mdx-components.ts
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-import type {MDXComponents} from "mdx/types";
-
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...components,
-  };
-}
