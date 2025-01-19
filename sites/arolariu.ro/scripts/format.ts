95e0bb2fcc45319d45143c4409b8e86e57264ff4
Alexandru-Razvan Olariu (from Dev Box) <aolariu@microsoft.com>
2025-01-19 20:48:57 +0200

    further work on scripts

--- sites/arolariu.ro/scripts/format.ts
+++ sites/arolariu.ro/scripts/format.ts
@@ -9,7 +9,7 @@ import path from "node:path";
  * It will use the `prettier.config.js` file as the configuration file.
  * If the configuration file does not exist, the script will exit with an error.
  */
-function formatCodeWithPrettier() {
+function formatCodeWithPrettier(verbose: boolean = false) {
   const prettierConfig = path.join(process.cwd(), "prettier.config.js");
   if (!fs.existsSync(prettierConfig)) {
     console.error("[arolariu::format] >>> The Prettier configuration file does not exist.");
@@ -26,11 +26,16 @@ function formatCodeWithPrettier() {
     console.error("[arolariu::format] >>> The code is not formatted with Prettier!.");
     console.log("[arolariu::format] >>> Formatting the code with Prettier...");
 
-    execSync(`npx prettier --write . --config-precedence prefer-file --cache --cache-strategy metadata`, {
-      stdio: "inherit",
-    });
-
-    console.log("[arolariu::format] >>> The code has been formatted with Prettier.");
+    try {
+      execSync(`npx prettier --write . --config-precedence prefer-file --cache --cache-strategy metadata`, {
+        stdio: "inherit",
+      });
+      console.log("[arolariu::format] >>> The code has been formatted with Prettier.");
+    } catch (error) {
+      verbose && console.error(error);
+      console.error("[arolariu::format] >>> There was an error formatting the code with Prettier.");
+      process.exit(1);
+    }
   } finally {
     console.log("[arolariu::format] >>> The code has been processed with Prettier.");
   }
@@ -39,9 +44,9 @@ function formatCodeWithPrettier() {
 /**
  * This is the function that will be executed when this script is ran.
  */
-export default async function main() {
+export default async function main(verbose: boolean = false) {
   console.info("[arolariu::format] Formatting the code...");
-  formatCodeWithPrettier();
+  formatCodeWithPrettier(verbose);
 }
 
 main();
