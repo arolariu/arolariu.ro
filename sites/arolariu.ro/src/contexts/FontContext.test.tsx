e16eaeb849e86dbeb681cf9ac3a6dfaea2b9a251
Alexandru-Razvan Olariu (from Dev Box) <aolariu@microsoft.com>
2025-01-22 13:23:56 +0200

    add tests for localStorage

--- sites/arolariu.ro/src/contexts/FontContext.test.tsx
+++ sites/arolariu.ro/src/contexts/FontContext.test.tsx
@@ -95,4 +95,142 @@ describe("useFontContext", () => {
     ).not.toThrow();
     consoleError.mockRestore();
   });
+
+  it("should get the font set from localStorage", async () => {
+    const localStorageMock = (function () {
+      let store: Record<string, string> = {};
+
+      return {
+        getItem: function (key: string) {
+          return store[key] || null;
+        },
+        setItem: function (key: string, value: string) {
+          store[key] = value.toString();
+        },
+        removeItem: function (key: string) {
+          delete store[key];
+        },
+        clear: function () {
+          store = {};
+        },
+      };
+    })();
+
+    Object.defineProperty(window, "localStorage", {
+      value: localStorageMock,
+    });
+
+    localStorageMock.setItem("selectedFont", JSON.stringify(customFont));
+
+    let getByTestId: (id: string) => HTMLElement;
+    await act(async () => {
+      const result = render(
+        <FontContextProvider>
+          <TestComponent />
+        </FontContextProvider>,
+      );
+      getByTestId = result.getByTestId;
+    });
+
+    await waitFor(() => expect(getByTestId("font").textContent).toEqual(JSON.stringify(customFont)));
+    await waitFor(() => expect(localStorageMock.getItem("selectedFont")).toEqual(JSON.stringify(customFont)));
+  });
+
+  it("should update the font in localStorage", async () => {
+    const localStorageMock = (function () {
+      let store: Record<string, string> = {};
+
+      return {
+        getItem: function (key: string) {
+          return store[key] || null;
+        },
+        setItem: function (key: string, value: string) {
+          store[key] = value.toString();
+        },
+        removeItem: function (key: string) {
+          delete store[key];
+        },
+        clear: function () {
+          store = {};
+        },
+      };
+    })();
+
+    Object.defineProperty(window, "localStorage", {
+      value: localStorageMock,
+    });
+
+    let getByText: (text: string) => HTMLElement;
+    await act(async () => {
+      const result = render(
+        <FontContextProvider>
+          <TestComponent />
+        </FontContextProvider>,
+      );
+      getByText = result.getByText;
+    });
+
+    const initialFont = localStorageMock.getItem("selectedFont");
+    expect(initialFont).toEqual(JSON.stringify(defaultFont));
+
+    await act(async () => {
+      getByText("Set Font").click();
+    });
+
+    await waitFor(() => expect(localStorageMock.getItem("selectedFont")).toEqual(JSON.stringify(customFont)));
+
+    const newFont = localStorageMock.getItem("selectedFont");
+    expect(newFont).toEqual(JSON.stringify(customFont));
+  });
+
+  it("should fetch the updated font from localStorage", async () => {
+    const localStorageMock = (function () {
+      let store: Record<string, string> = {};
+
+      return {
+        getItem: function (key: string) {
+          return store[key] || null;
+        },
+        setItem: function (key: string, value: string) {
+          store[key] = value.toString();
+        },
+        removeItem: function (key: string) {
+          delete store[key];
+        },
+        clear: function () {
+          store = {};
+        },
+      };
+    })();
+
+    Object.defineProperty(window, "localStorage", {
+      value: localStorageMock,
+    });
+
+    localStorageMock.setItem("selectedFont", JSON.stringify(defaultFont));
+
+    let getByTestId: (id: string) => HTMLElement;
+    await act(async () => {
+      const result = render(
+        <FontContextProvider>
+          <TestComponent />
+        </FontContextProvider>,
+      );
+      getByTestId = result.getByTestId;
+    });
+
+    await waitFor(() => expect(getByTestId("font").textContent).toEqual(JSON.stringify(defaultFont)));
+    const initialFont = localStorageMock.getItem("selectedFont");
+    expect(initialFont).toEqual(JSON.stringify(defaultFont));
+
+    // Set custom font
+    await act(async () => {
+      getByTestId("font").click();
+    });
+
+    // Confirm custom font
+    await waitFor(() => expect(getByTestId("font").textContent).toEqual(JSON.stringify(customFont)));
+    const receivedFont = localStorageMock.getItem("selectedFont");
+    expect(receivedFont).toEqual(JSON.stringify(customFont));
+  });
 });
