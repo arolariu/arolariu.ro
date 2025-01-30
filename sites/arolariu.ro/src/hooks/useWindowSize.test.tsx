ee131f9ed3f9db15570fa2c8358373ce3ff5976a
Alexandru-Razvan Olariu (from Dev Box) <aolariu@microsoft.com>
2025-01-30 17:01:02 +0200

    minor fixes

--- sites/arolariu.ro/src/hooks/useWindowSize.test.tsx
+++ sites/arolariu.ro/src/hooks/useWindowSize.test.tsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import {act, renderHook} from "@testing-library/react";
-import {useWindowSize} from "./index";
+import {useWindowSize} from "./useWindowSize";
 
 describe("useWindowSize", () => {
   beforeEach(() => {
