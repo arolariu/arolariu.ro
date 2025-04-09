7d76264ff3d73ac9710a939042a3e76367315d3a
Alexandru-Razvan Olariu (from Dev Box) <aolariu@microsoft.com>
2025-04-09 21:07:46 +0300

    comments

--- sites/arolariu.ro/scripts/afterBuild.ts
+++ sites/arolariu.ro/scripts/afterBuild.ts
@@ -1,10 +1,5 @@
 /** @format */
 
-/**
- * This script will run after the build process finishes.
- * It will run a couple of subsequent scripts to perform the following tasks:
- */
-
 export async function main() {
   console.info("[arolariu.ro::afterBuild] Running after build scripts...");
 
--- sites/arolariu.ro/scripts/beforeBuild.ts
+++ sites/arolariu.ro/scripts/beforeBuild.ts
@@ -1,7 +1,14 @@
 /** @format */
 
 /**
- * This script will run before the build process starts.
+ * Executes a series of scripts to prepare for the build process.
+ *
+ * This function performs the following operations sequentially:
+ * 1. Cleans the build directory using the clean script
+ * 2. Formats the code using the format script
+ * 3. Generates new licenses and acknowledgements
+ *
+ * Each step is logged to the console with appropriate start and completion messages.
  */
 export default async function main() {
   console.info("[arolariu.ro::beforeBuild] Running before build scripts...");
@@ -18,7 +25,7 @@ export default async function main() {
     console.info("[arolariu.ro::beforeBuild] Finished formatting code.");
   });
 
-  // 3. Generate new licenses & acknowledegmenets using the generateAcknowledgements script
+  // 3. Generate new licenses & acknowledgements using the generateAcknowledgements script
   console.info("[arolariu.ro::beforeBuild] Generating licenses...");
   await import("./generateAcknowledgements").then(() => {
     console.info("[arolariu.ro::beforeBuild] Finished generating licenses.");
@@ -28,3 +35,4 @@ export default async function main() {
 }
 
 main();
+
