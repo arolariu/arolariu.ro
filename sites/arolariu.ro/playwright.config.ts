9551ae515c4f07518ef2d7e23c7e8c705e773eb4
Alexandru-Razvan Olariu (from Dev Box) <aolariu@microsoft.com>
2025-05-13 15:21:21 +0300

    diff playwright config in CI

--- sites/arolariu.ro/playwright.config.ts
+++ sites/arolariu.ro/playwright.config.ts
@@ -2,43 +2,57 @@
 
 import {defineConfig, devices} from "@playwright/test";
 
+const weAreInCI = Boolean(process.env["CI"]);
+
+const allProjects = [
+  {
+    name: "chromium-desktop-e2e",
+    use: {
+      ...devices["Desktop Chrome"],
+      baseURL: "https://localhost:3000",
+      ignoreHTTPSErrors: true,
+    },
+    timeout: 60 * 1000, // 60 seconds - 1 minute
+    testMatch: "src/**/*.spec.{ts,tsx}",
+  },
+  {
+    name: "firefox-desktop-e2e",
+    use: {
+      ...devices["Desktop Firefox"],
+      baseURL: "https://localhost:3000",
+      ignoreHTTPSErrors: true,
+    },
+    timeout: 60 * 1000, // 60 seconds - 1 minute
+    testMatch: "src/**/*.spec.{ts,tsx}",
+  },
+  {
+    name: "webkit-desktop-e2e",
+    use: {
+      ...devices["Desktop Safari"],
+      baseURL: "https://localhost:3000",
+      ignoreHTTPSErrors: true,
+    },
+    timeout: 60 * 1000, // 60 seconds - 1 minute
+    testMatch: "src/**/*.spec.{ts,tsx}",
+  },
+];
+
+const projectsToRun = allProjects.filter((project) => {
+  if (weAreInCI) {
+    return (
+      project.name !== "webkit-desktop-e2e" // Skip WebKit in CI
+      && project.name !== "firefox-desktop-e2e" // Skip Firefox in CI
+    );
+  }
+  return true; // Run all projects locally
+});
+
 export default defineConfig({
   fullyParallel: true,
   retries: 1,
-  reporter: "html",
+  reporter: weAreInCI ? "github" : "html",
 
-  projects: [
-    {
-      name: "firefox-desktop-e2e",
-      use: {
-        ...devices["Desktop Firefox"],
-        baseURL: "https://localhost:3000",
-        ignoreHTTPSErrors: true,
-      },
-      timeout: 60 * 1000, // 60 seconds - 1 minute
-      testMatch: "src/**/*.spec.{ts,tsx}",
-    },
-    {
-      name: "chromium-desktop-e2e",
-      use: {
-        ...devices["Desktop Chrome"],
-        baseURL: "https://localhost:3000",
-        ignoreHTTPSErrors: true,
-      },
-      timeout: 60 * 1000, // 60 seconds - 1 minute
-      testMatch: "src/**/*.spec.{ts,tsx}",
-    },
-    {
-      name: "webkit-desktop-e2e",
-      use: {
-        ...devices["Desktop Safari"],
-        baseURL: "https://localhost:3000",
-        ignoreHTTPSErrors: true,
-      },
-      timeout: 60 * 1000, // 60 seconds - 1 minute
-      testMatch: "src/**/*.spec.{ts,tsx}",
-    },
-  ],
+  projects: projectsToRun,
 
   webServer: [
     {
