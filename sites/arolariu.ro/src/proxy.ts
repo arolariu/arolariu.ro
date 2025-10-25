e6a6001971da39898e66142414d8752e8e456edf
Alexandru-Razvan Olariu (from Dev Box) <aolariu@microsoft.com>
2025-10-25 12:38:04 +0300

    rename middleware to proxy

--- sites/arolariu.ro/src/proxy.ts
+++ sites/arolariu.ro/src/proxy.ts
@@ -13,8 +13,10 @@ export const config = {
     // Skip Next.js internals and all static files, unless found in search params
     // eslint-disable-next-line unicorn/prefer-string-raw -- this is a regex
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+
     // Always run for API routes
     "/(api|trpc)(.*)",
+
     // Skip for static files in the /manifest or /_manifest folder
     "/(manifest|_manifest)(.*)",
   ],
