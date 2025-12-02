659bbdd80dede5c47a11f5decffe45cecda82b12
Alexandru-Razvan Olariu (from Dev Box) <aolariu@microsoft.com>
2025-12-02 16:39:16 +0200

    partial work in invoice header

--- sites/arolariu.ro/src/app/domains/invoices/view-invoice/[id]/_components/InvoiceHeader.tsx
+++ sites/arolariu.ro/src/app/domains/invoices/view-invoice/[id]/_components/InvoiceHeader.tsx
@@ -3,6 +3,7 @@
 import type {Invoice} from "@/types/invoices";
 import {Button, Input, Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@arolariu/components";
 import {motion} from "motion/react";
+import {useCallback, useState} from "react";
 import {TbPrinter} from "react-icons/tb";
 
 type Props = {
@@ -14,7 +15,17 @@ type Props = {
  * It includes the invoice ID, name, a badge for important invoices, and a print button.
  * @returns The InvoiceHeader component, CSR'ed.
  */
-export default function InvoiceHeader({invoice}: Readonly<Props>) {
+export default function InvoiceHeader({invoice}: Readonly<Props>): React.JSX.Element {
+  const [invoiceName, setInvoiceName] = useState<string>(invoice.name);
+
+  const handleNameChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setInvoiceName(event.target.value);
+  }, []);
+
+  const handleInvoicePrint = useCallback(() => {
+    globalThis.window.print();
+  }, []);
+
   return (
     <motion.div
       initial={{opacity: 0, y: -20}}
@@ -25,7 +36,8 @@ export default function InvoiceHeader({invoice}: Readonly<Props>) {
         <div>
           <Input
             type='text'
-            value={invoice.name}
+            value={invoiceName}
+            onChange={handleNameChange}
             className='w-full border-0 text-3xl font-bold tracking-tight focus-visible:border-0 focus-visible:ring-0'
           />
           <p className='text-gray-300 dark:text-gray-800'>{invoice.id}</p>
@@ -39,8 +51,7 @@ export default function InvoiceHeader({invoice}: Readonly<Props>) {
                 className='cursor-pointer'
                 variant='outline'
                 size='sm'
-                // eslint-disable-next-line react/jsx-handler-names -- not applicable.
-                onClick={globalThis.window.print}>
+                onClick={handleInvoicePrint}>
                 <TbPrinter className='mr-2 h-4 w-4' />
                 Print
               </Button>
