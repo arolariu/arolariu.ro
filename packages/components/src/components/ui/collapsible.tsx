8ea80ec1bd867ba2d88dc34dd14c98a39ccc1bc8
Alexandru-Razvan Olariu (from Dev Box) <aolariu@microsoft.com>
2025-12-07 22:04:04 +0200

    fix linting

--- packages/components/src/components/ui/collapsible.tsx
+++ packages/components/src/components/ui/collapsible.tsx
@@ -4,8 +4,6 @@ import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
 
 const Collapsible = CollapsiblePrimitive.Root;
 
-const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
-
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
+const {CollapsibleTrigger, CollapsibleContent} = CollapsiblePrimitive;
 
 export {Collapsible, CollapsibleContent, CollapsibleTrigger};
