2578fd18a15aafbe8537179376a379947a09b2b7
Alexandru-Razvan Olariu (from Dev Box) <aolariu@microsoft.com>
2025-12-22 23:54:03 +0200

    add tooltips for view actions

--- sites/arolariu.ro/src/app/domains/invoices/view-invoices/_components/tables/TableViewActions.tsx
+++ sites/arolariu.ro/src/app/domains/invoices/view-invoices/_components/tables/TableViewActions.tsx
@@ -8,6 +8,10 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
 } from "@arolariu/components";
 import Link from "next/link";
 import {TbEdit, TbMenu3, TbShare, TbTrash} from "react-icons/tb";
@@ -25,42 +29,61 @@ export default function TableViewActions({invoice}: Readonly<Props>): React.JSX.
   const {open: openDeleteDialog} = useDialog("SHARED__INVOICE_DELETE", "delete", {invoice});
 
   return (
-    <DropdownMenu modal={false}>
-      <DropdownMenuTrigger
-        asChild
-        className='cursor-pointer hover:text-blue-500'>
-        <Button
-          variant='ghost'
-          size='icon'
-          className='bg-background/80 h-8 w-8 print:hidden'>
-          <TbMenu3 className='h-4 w-4' />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent
-        align='end'
-        className='w-[160px]'>
-        <DropdownMenuItem className='cursor-pointer'>
-          <Link
-            href={`/domains/invoices/edit-invoice/${invoice.id}`}
-            className='flex items-center gap-2'>
-            <TbEdit className='mr-2 h-4 w-4' />
-            Edit
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={openShareDialog}
-          className='cursor-pointer'>
-          <TbShare className='mr-2 h-4 w-4' />
-          Share
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem
-          className='text-destructive cursor-pointer'
-          onClick={openDeleteDialog}>
-          <TbTrash className='mr-2 h-4 w-4' />
-          Delete
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <TooltipProvider>
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger
+          asChild
+          className='cursor-pointer hover:text-blue-500'>
+          <Button
+            variant='ghost'
+            size='icon'
+            className='bg-background/80 h-8 w-8 print:hidden'>
+            <TbMenu3 className='h-4 w-4' />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          align='end'
+          className='w-40'>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <DropdownMenuItem
+                asChild
+                className='cursor-pointer'>
+                <Link
+                  href={`/domains/invoices/edit-invoice/${invoice.id}`}
+                  className='flex items-center gap-2'>
+                  <TbEdit className='mr-2 h-4 w-4' />
+                  Edit
+                </Link>
+              </DropdownMenuItem>
+            </TooltipTrigger>
+            <TooltipContent side='left'>Edit invoice details</TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <DropdownMenuItem
+                onClick={openShareDialog}
+                className='cursor-pointer'>
+                <TbShare className='mr-2 h-4 w-4' />
+                Share
+              </DropdownMenuItem>
+            </TooltipTrigger>
+            <TooltipContent side='left'>Share invoice via link or email</TooltipContent>
+          </Tooltip>
+          <DropdownMenuSeparator />
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <DropdownMenuItem
+                className='text-destructive cursor-pointer'
+                onClick={openDeleteDialog}>
+                <TbTrash className='mr-2 h-4 w-4' />
+                Delete
+              </DropdownMenuItem>
+            </TooltipTrigger>
+            <TooltipContent side='left'>Permanently delete this invoice</TooltipContent>
+          </Tooltip>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </TooltipProvider>
   );
 }
